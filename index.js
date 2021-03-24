32d3c233321fa37337bd884c135eabdc787ca6d0
Josh Pollmann <joshpollmann102@gmail.com>
2021-03-24 10:05:15 -0400

    Part 8, 8.4

--- index.js
+++ index.js
@@ -101,9 +101,9 @@ const typeDefs = gql`
   }
 
   type Query {
-    bookCount(name:String): Int!
+    bookCount(name: String): Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -112,7 +112,10 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if(!args.author) return books;
+      return books.filter(b => b.author === args.author);
+    },
     allAuthors: () => authors
   },
   Author: {
